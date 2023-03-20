631e7b6ae308e0755cfc8c787904744974c8bdf6
gjoivan <gorgievskii@yahoo.com>
2023-03-20 20:13:03 +0100

    test

--- config/index.php
+++ config/index.php
@@ -3,7 +3,7 @@ $path = "https://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
 
 $hostname="localhost";
 $username="root";
-$pass_word="12345678";
+$pass_word="";
 $database_name="NikoSan";
 
 $mysqliObj = new mysqli($hostname, $username, $pass_word, $database_name);
