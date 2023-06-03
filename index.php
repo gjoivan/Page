3dd34fb3335a610467170aae1716e5c4d3a2c893
gjoivan <gorgievskii@yahoo.com>
2023-06-03 17:26:51 +0200

    changes

--- index.php
+++ index.php
@@ -2,78 +2,60 @@
 
 require_once __DIR__."/config/index.php";
 require_once  __DIR__."/config/login_session.php";
-$route = isset($_GET['route']) ? $_GET['route'] : 'home';
-var_dump($route);
 ?>
-
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Gradba Unikat</title>
-    <link rel="stylesheet" href="./static/css/bootstrap.min.css" />
-    <link rel="stylesheet" href="./static/css/bootstrap-grid.min.css" />
-    <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-    <!-- jQuery library -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
-    <!-- Latest compiled JavaScript -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-    <script href="jquery-3.6.0.js"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-    <meta name="description" content="">
-    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
-    <meta name="generator" content="Hugo 0.84.0">
-    <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/dashboard/">
+<html lang="en"> 
+  <?php
+  $route = isset($_GET['route']) ? $_GET['route'] : 'home';
+  $routes_allowed = array('home', 'employees');
+  include_once('./header.php');
+  include_once('./nav.php');
+  if(in_array($route, $routes_allowed)){
+    include_once("./{$route}.php");
+  }
+  ?>
+  <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Gradba Unikat</title>
+      <link rel="stylesheet" href="./static/css/bootstrap.min.css" />
+      <link rel="stylesheet" href="./static/css/bootstrap-grid.min.css" />
+      <!-- Latest compiled and minified CSS -->
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+      <!-- jQuery library -->
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
+      <!-- Latest compiled JavaScript -->
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+      <script href="jquery-3.6.0.js"></script>
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+      <meta name="description" content="">
+      <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+      <meta name="generator" content="Hugo 0.84.0">
+      <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/dashboard/">
 
-    <!-- Bootstrap core CSS -->
-    <link href="../static/css/bootstrap.min.css" rel="stylesheet">
-    <style>
-      .bd-placeholder-img {
-        font-size: 1.125rem;
-        text-anchor: middle;
-        -webkit-user-select: none;
-        -moz-user-select: none;
-        user-select: none;
-      }
-
-      @media (min-width: 768px) {
-        .bd-placeholder-img-lg {
-          font-size: 3.5rem;
+      <!-- Bootstrap core CSS -->
+      <link href="../static/css/bootstrap.min.css" rel="stylesheet">
+      <style>
+        .bd-placeholder-img {
+          font-size: 1.125rem;
+          text-anchor: middle;
+          -webkit-user-select: none;
+          -moz-user-select: none;
+          user-select: none;
         }
-      }
-    </style>
-  
-</head>
-<body>
-    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NBRVWPD"
-    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTMKCTSNJC"
-    height="0" width="0"
-    style="display:none;visibility:hidden"></iframe></noscript>
 
-    <script src="../assets/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/feather-icons@4.28.0/dist/feather.min.js" integrity="sha384-uO3SXW5IuS1ZpFPKugNNWqTZRRglnUJK6UAZ/gxOX80nxEkN9NcGZTftn6RzhGWE" crossorigin="anonymous"></script><script src="https://cdn.jsdelivr.net/npm/chart.js@2.9.4/dist/Chart.min.js" integrity="sha384-zNy6FEbO50N+Cg5wap8IKA4M/ZnLJgzc6w2NqACZaK0u0FXfOWRRJOnQtpZun8ha" crossorigin="anonymous"></script><script src="dashboard.js"></script>
- 
-    <?php 
-    include_once('./header.php');
-    if($route != 'register') include_once('./nav.php');
-    if($route == 'login'){
-        include_once('./login.php');
-    }elseif($route == "register"){
-        include_once("./register.php");
-    }elseif($route == "main"){
-        include_once("./main.php");
-    }elseif($route == 'contact'){
-        include_once('./contact.php');
-    }else{
-        include('./home.php');
-    }
-    ?>
+        @media (min-width: 768px) {
+          .bd-placeholder-img-lg {
+            font-size: 3.5rem;
+          }
+        }
+      </style>
+    
+  </head>
+  <body>
 
-</body>
+  </body>
 </html>
 
 
